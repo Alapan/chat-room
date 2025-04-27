fca973c633660168e0096657b431b99e77cbd016
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-04-27 16:43:10 +0100

    Refs #9 Update styles on home page to use logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
 export const metadata: Metadata = {
-  title: 'Chat Room',
+  title: 'ChatHub',
   description: 'A simple chat room application',
 };
 
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
