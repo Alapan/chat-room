a9fcb7c22655fe2fe8761d95b14f7ffd21037de9
Abhiroop Santra <abhiroop.santra@gmail.com>
2025-04-29 14:12:56 +0400

    feat: added multiline text, send button, shift+enter for new line, ctrl/cmd+enter to send

--- src/app/components/InputMessageBox.tsx
+++ src/app/components/InputMessageBox.tsx
@@ -2,10 +2,12 @@
 
 import { KeyboardEvent, useState } from 'react';
 import EmojiPicker from 'emoji-picker-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons/faPaperPlane";
 
 interface InputMessageBoxProps {
-  onClick: () => void;
-};
+  onClick: (message: string) => void;
+}
 
 export const InputMessageBox = ({
   onClick
@@ -14,9 +16,16 @@ export const InputMessageBox = ({
   const [ showPicker, setShowPicker ] = useState(false);
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.key === 'Enter' && message.trim() !== '') {
+    if (event.key === 'Enter' && !event.shiftKey && message.trim() !== '') {
       event.preventDefault();
-      onClick();
+      onClick(message);
+      setMessage('');
+    }  else if (
+        (event.key === 'Enter' && event.ctrlKey) ||
+        (event.key === 'Enter' && event.metaKey)
+    ) {
+      event.preventDefault();
+      onClick(message);
       setMessage('');
     }
   };
@@ -26,23 +35,32 @@ export const InputMessageBox = ({
     setShowPicker(false);
   };
 
+  const handleSendClick = () => {
+    if(message.trim() !== '') {
+      onClick(message);
+      setMessage('');
+    }
+  };
+
   return (
     <div className='flex items-center justify-between p-4 rounded-md shadow-md'>
-      <input
-        type='text'
-        placeholder='Type your message here...'
-        aria-label='Message input'
-        className='w-full p-2 rounded-md border border-green-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+      <textarea
+          placeholder='Type your message here...'
+          aria-label='Message input'
+          className='w-full p-2 rounded-md border border-green-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent resize-none'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
+          rows={1}
+      ></textarea>
       <button
         type='button'
         className='bg-green text-white p-2 rounded focus:outline-none cursor-pointer'
         onClick={() => setShowPicker(!showPicker)}
       >😊</button>
       {showPicker && <EmojiPicker onEmojiClick={handleEmojiClick} />}
+      <button type='button' className='bg-green text-white px-4 py-2 rounded focus:outline-none cursor-pointer'
+              onClick={handleSendClick}><FontAwesomeIcon icon={faPaperPlane} /></button>
     </div>
   );
 }
