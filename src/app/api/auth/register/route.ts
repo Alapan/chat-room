91842bcd7b5f0469ed54fc95f9fe434ba04a3d05
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-05-03 11:13:24 +0400

    fix: added declarative model validations

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {PrismaClient} from "@prisma/client";
 import bcrypt from "bcrypt";
+import {z} from "zod";
 
 interface RegisterUser {
     name: string;
@@ -11,13 +12,57 @@ interface RegisterUser {
 
 const prisma = new PrismaClient();
 
+// model validation rules
+const registerUserSchema = z.object({
+    name: z.string()
+        .nonempty({message: "Name is required"})
+        .min(5, {message: "Name must be at least 5 characters long"})
+        .max(50, {message: "Name must be at most 50 characters long"}),
+    email: z.string()
+        .nonempty({message: "Email is required"})
+        .email({message: "Must be a valid email address"})
+        .max(50, {message: "Email must be at most 50 characters long"}),
+    password: z.string()
+        .nonempty({message: "Password is required"})
+        .min(6, {message: "Password must be at least 5 characters long"})
+        .max(15, {message: "Password must be at most 15 characters long"})
+        .refine((password) => /[A-Z]/.test(password),
+            {message: "Password must be include at least 1 uppercase letter"})
+        .refine((password) => /[a-z]/.test(password),
+            {message: "Password must be include at least 1 lowercase letter"})
+        .refine((password) => /[0-9]/.test(password),
+            {message: "Password must be include at least 1 number"})
+        .refine((password) => /[!@#$%^&*]/.test(password), {
+            message: "Password must be include at least 1 of these special characters: !@#$%^&*",
+        }),
+    confirmPassword: z.string()
+        .nonempty({message: "Confirm Password is required"})
+        .min(6, {message: "Confirm Password must be at least 5 characters long"})
+        .max(15, {message: "Confirm Password must be at most 15 characters long"})
+}).superRefine(({confirmPassword, password}, ctx) => {
+    if (confirmPassword !== password) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Confirm Password must be same as Password",
+            path: ["confirmPassword"],
+        })
+    }
+});
+
 export async function POST(request: NextRequest) {
     try {
         const body: RegisterUser = await request.json();
 
-        // validate fields
-        if (!body || !body.email || !body.password || !body.confirmPassword || !body.name) {
+        // validate payload
+        if (!body) {
             return NextResponse.json({error: "Registration Failed. Please provide all required fields."}, {status: 400});
+        }
+
+        // model validation
+        const validationResult = registerUserSchema.safeParse(body);
+        if (!validationResult.success) {
+            const errors = validationResult.error.errors.map((error) => error.message);
+            return NextResponse.json({error: errors}, {status: 400});
         }
 
         // check if password and confirm password are same
--- src/app/api/chatrooms/[id]/chats/route.ts
+++ src/app/api/chatrooms/[id]/chats/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import {PrismaClient} from "@prisma/client";
+import {z} from "zod";
 
 
 interface ChatMessage {
@@ -10,15 +11,23 @@ interface ChatMessage {
     sentAt: Date;
 }
 
+const postMessageSchema = z.object({
+    chatRoomId: z.number().int(),
+    userId: z.number().int(),
+    text: z.string()
+        .nonempty({message: "Message cannot be empty"})
+        .max(2000, {message: "Message cannot exceed 2000 characters"}),
+});
+
 export async function GET(_: NextRequest, context: { params: { id: string } }) {
     try {
-        const { id } = await context.params;
+        const {id} = await context.params;
         const prisma = new PrismaClient();
         if (!id) {
-            return NextResponse.json({ error: 'Chat room ID is required' }, { status: 400 });
+            return NextResponse.json({error: 'Chat room ID is required'}, {status: 400});
         }
-        const chatMessages : ChatMessage[] = await prisma.message.findMany({
-            where: { chatRoomId: parseInt(id) },
+        const chatMessages: ChatMessage[] = await prisma.message.findMany({
+            where: {chatRoomId: parseInt(id)},
             select: {
                 id: true,
                 chatRoomId: true,
@@ -28,28 +37,38 @@ export async function GET(_: NextRequest, context: { params: { id: string } }) {
             },
         });
         if (!chatMessages) {
-            return NextResponse.json({ error: 'No messages found' }, { status: 404 });
+            return NextResponse.json({error: 'No messages found'}, {status: 404});
         }
         return NextResponse.json(chatMessages);
     } catch (error) {
         console.log(error);
-        return NextResponse.json({ error: 'Fetching chat messages failed' }, { status: 500 });
+        return NextResponse.json({error: 'Fetching chat messages failed'}, {status: 500});
     }
 }
 
 export async function POST(request: NextRequest, context: { params: { id: string } }) {
     try {
-        const { id } = await context.params;
+        const {id} = await context.params;
         const prisma = new PrismaClient();
-        if (!id) {
-            return NextResponse.json({ error: 'Chat room ID is required' }, { status: 400 });
-        }
+
+        // if (!id) {
+        //     return NextResponse.json({error: 'Chat room ID is required'}, {status: 400});
+        // }
+
         const body: ChatMessage = await request.json();
 
-        if (!body || !body.userId || !body.text) {
-            return NextResponse.json({ error: 'User ID and message text are required' }, { status: 400 });
+        // Validate payload
+        if (!body) {
+            return NextResponse.json({error: 'Request body cannot be empty'}, {status: 400});
         }
 
+        // Model validation
+        const validationResult = postMessageSchema.safeParse(body);
+        if (!validationResult.success) {
+            const errors = validationResult.error.errors.map((error) => error.message);
+            return NextResponse.json({error: errors}, {status: 400});
+        }
+        
         const newMessage = await prisma.message.create({
             data: {
                 chatRoomId: parseInt(id),
@@ -62,6 +81,6 @@ export async function POST(request: NextRequest, context: { params: { id: string
         return NextResponse.json(newMessage);
     } catch (error) {
         console.log(error);
-        return NextResponse.json({ error: 'Sending message failed' }, { status: 500 });
+        return NextResponse.json({error: 'Sending message failed'}, {status: 500});
     }
 }
